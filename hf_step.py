8149f262013e2f23f8506b65fd6e76d451093e60
Banana_coder <ambadiritik@gmail.com>
2020-09-18 22:49:48 +0530

    Added Code Generator

--- hf_step.py
+++ hf_step.py
@@ -18,6 +18,8 @@ class HeapNode:
         return self.freq < other.freq
 
     def __eq__(self, other):
+        if other == None:
+            return False
         return self.freq == other.freq
 
     def __repr__(self):
@@ -28,6 +30,7 @@ class HuffmanEncoder:
     def __init__(self, text):
         self.text = text
         self.heap = []
+        self.codes = {}
 
     def build_freq_dict(self):
         frequency = {}
@@ -50,15 +53,27 @@ class HuffmanEncoder:
             merged.left = x
             merged.right = y
             heapq.heappush(self.heap, merged)
-            print(self.heap)
+
+    def traverse_tree(self, node, bin_code):
+        if node == None:
+            return
+        if node.char != None:
+            self.codes[node.char] = bin_code
+        self.traverse_tree(node.left, bin_code + "0")
+        self.traverse_tree(node.right, bin_code + "1")
+
+    def generate_code(self):
+        root = heapq.heappop(self.heap)
+        bin_code = ""
+        self.traverse_tree(root, bin_code)
 
     def compress(self):
         frequency = self.build_freq_dict()
         self.build_min_heap(frequency)
-        print(self.heap)
         self.generate_tree()
+        self.generate_code()
         print(frequency)
-        print(self.heap)
+        print(self.codes)
 
 
 t_input = str(input())
